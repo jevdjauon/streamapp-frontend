02b70b97fa3dd3d5ceb1b9bd5f54b12d9e3503ad
jevdjauon <aleksandar@uon.rs>
2020-08-07 19:01:52 +0200

    test

---
+++ build/precache-manifest.774e9efdca924e88d45972488ce2ae52.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "897de1613c7f9e1915891544fd9bdf07",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2b23850d2e6f192acb73",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9a2f0490abe34bc0a010",
+    "url": "/static/js/2.ff4f381a.chunk.js"
+  },
+  {
+    "revision": "4651a60dd24a3d76949974fa9e435183",
+    "url": "/static/js/2.ff4f381a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b23850d2e6f192acb73",
+    "url": "/static/js/main.d5888c5f.chunk.js"
+  },
+  {
+    "revision": "08b0b9b60d2eaf84c65e",
+    "url": "/static/js/runtime-main.d170e39f.js"
+  }
+]);
--- src/components/HeroesList.js
+++ src/components/HeroesList.js
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
-import axios from "axios";
+// import axios from "axios";
 
 export default class HeroesList extends Component {
-  constructor(props) {
-    super(props);
-  }
+  // constructor(props) {
+  //   super(props);
+  // }
 
   render() {
     return <div></div>;
